89f6c80fc8bcc2f484322c7c9135bd8d24fe1d6d
Julia Chae <julia.chae2000@gmail.com>
2025-02-27 11:27:57 -0500

    Update download_benchmarks.sh
    removing df2 direct download

--- dataset/download_benchmarks.sh
+++ dataset/download_benchmarks.sh
@@ -4,17 +4,14 @@
 mkdir -p ./data
 cd data
 
-# Download datasets
+# Download pods and dogs datasets
 echo "Downloading zips"
 wget -O pods.zip https://data.csail.mit.edu/personal_rep/pods.zip
 wget -O dogs.zip https://data.csail.mit.edu/personal_rep/dogs.zip
-wget -O df2.zip https://data.csail.mit.edu/personal_rep/df2.zip
 
 # Unzip
 echo "Unzipping data"
 unzip pods.zip
 unzip dogs.zip
-unzip df2.zip
 rm pods.zip
 rm dogs.zip
-rm df2.zip
